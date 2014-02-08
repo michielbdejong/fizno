697f1ce3de01054679f514500f4c5b6d82e7106f
Michiel B. de Jong <michiel@unhosted.org>
2014-02-08 18:41:44 +0000

    rudimentary folder listings

--- docker.js
+++ docker.js
@@ -1,6 +1,22 @@
-var http = require('http'),
+var knowledgeDir = './knowledge/',
+  http = require('http'),
   nodestatic = require('node-static'),
-  file = new nodestatic.Server('./');
+  fs = require('fs'),
+  file = new nodestatic.Server(knowledgeDir);
 http.createServer(function(req, res) {
-  file.serve(req, res);
+  if (req.url.substr(-1) === '/') {
+    fs.readdir(knowledgeDir+req.url.replace('..', ''), function(err, list) {
+      var i;
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      res.write('<ul>');
+      for(i=0; i<list.length; i++) {
+        res.write('<li><a href="'+list[i]+'">'+list[i]+'</a>');
+      }
+      res.end('</ul>');
+    });
+  } else {
+    file.serve(req, res);
+  }
 }).listen(49080);
