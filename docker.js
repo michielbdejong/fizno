aac955ae8e2d5b28c1a69f2c79e3d612721ed884
Michiel B. de Jong <michiel@unhosted.org>
2014-02-08 08:23:58 +0000

    update logo

--- docker.js
+++ docker.js
@@ -1,7 +1,6 @@
 var http = require('http'),
   nodestatic = require('node-static'),
-  file = new nodestatic.Server('./public/');
-  //res.end('<iframe width="640" height="390" src="//www.youtube.com/embed/AuX7nPBqDts" frameborder="0" allowfullscreen></iframe>');
+  file = new nodestatic.Server('./');
 http.createServer(function(req, res) {
   file.serve(req, res);
 }).listen(49080);
